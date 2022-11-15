5a54f30d6d609b6494af63e55e4fd725708113eb
kennn21 <91940623+kennn21@users.noreply.github.com>
2022-11-15 09:25:21 +0700

    Paginations done

---
+++ app/src/main/java/com/example/projecta/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.projecta
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.projecta.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.pageTitle.setOnClickListener{
+            Toast.makeText(activity, "Hi", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/projecta/MainActivity.kt
+++ app/src/main/java/com/example/projecta/MainActivity.kt
@@ -3,49 +3,63 @@ package com.example.projecta
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.projecta.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     lateinit var binding: ActivityMainBinding
     lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.apply{
-            toggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open, R.string.close)
-            drawerLayout.addDrawerListener(toggle)
-            toggle.syncState()
 
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-            navView.setNavigationItemSelectedListener {
-                when(it.itemId){
-                    R.id.firstItem->{
-                        Toast.makeText(this@MainActivity, "Home Page", Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(this@MainActivity, MainActivity::class.java))
-                    }
-                    R.id.secondItem->{
-                        Toast.makeText(this@MainActivity, "User Profile Page", Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(this@MainActivity, userprofile::class.java))
-                    }
-                    R.id.thirdItem->{
-                        Toast.makeText(this@MainActivity, "Ti", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                true
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            it.isChecked = true
+            replaceFragment(HomeFragment(), it.title.toString())
+            when (it.itemId) {
+                R.id.firstItem -> replaceFragment(HomeFragment(), it.title.toString())
+                R.id.secondItem -> replaceFragment(UserProfileFragment(), it.title.toString())
+                R.id.thirdItem -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Settings",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+            true
         }
+    }
 
+    private fun replaceFragment(fragment: Fragment, title: String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+        binding.drawerLayout.closeDrawers()
+        setTitle(title)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(toggle.onOptionsItemSelected(item)){
-            true
+            return true
+        } else{
+            return super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
     }
 }
