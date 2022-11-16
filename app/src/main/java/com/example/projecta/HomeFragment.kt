d6aa546969ff5a79401f58c857ee5f2f5165cd93
kennn21 <91940623+kennn21@users.noreply.github.com>
2022-11-16 14:37:55 +0700

    Paginations done Functions done: - Location & Text2speech

--- app/src/main/java/com/example/projecta/HomeFragment.kt
+++ app/src/main/java/com/example/projecta/HomeFragment.kt
@@ -1,31 +1,158 @@
 package com.example.projecta
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.example.projecta.databinding.FragmentHomeBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import java.io.IOException
+import java.util.*
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var appContext: Context
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
     }
 
+
+    var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    var t1: TextToSpeech? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.pageTitle.setOnClickListener{
-            Toast.makeText(activity, "Hi", Toast.LENGTH_SHORT).show()
+        appContext = context?.applicationContext!!
+
+        var button = binding.btn
+        var button2 = binding.btn2
+//        var button3 = binding.back
+        var address = binding.address
+
+        t1 = TextToSpeech(appContext,
+            OnInitListener { i -> if (i != TextToSpeech.ERROR) t1?.setLanguage(Locale("id", "ID")) })
+
+        //Initialize fusedLocationProviderClient
+
+        //Initialize fusedLocationProviderClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(appContext)
+
+        button.setOnClickListener(View.OnClickListener { //Check permission
+
+            if (
+                    ActivityCompat.checkSelfPermission(
+                        appContext
+                        ,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    appContext,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                getLocation()
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    100
+                )
+            }
+        })
+
+        button2.setOnClickListener(View.OnClickListener {
+            val text: String = address.getText().toString()
+            t1!!.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+        })
+
+//        button3.setOnClickListener(View.OnClickListener {
+//            startActivity(
+//                Intent(
+//                    requireActivity(),
+//                    MainActivity::class.java
+//                )
+//            )
+//        })
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //Check condition
+        if (requestCode == 100 && grantResults.size > 0 && (grantResults[0] + grantResults[1]
+                    == PackageManager.PERMISSION_GRANTED)
+        ) {
+            getLocation()
+        } else {
+            Toast.makeText(appContext, "Permission denied.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        fusedLocationProviderClient?.getLastLocation()
+            ?.addOnCompleteListener(object : OnCompleteListener<Location?> {
+                override fun onComplete(task: Task<Location?>) {
+                    //Initialize location
+                    val location: Location? = task.getResult()
+                    if (location != null) {
+                        try {
+                            //Initialize geoCoder
+                            val geocoder = Geocoder(
+                                appContext,
+                                Locale.getDefault()
+                            )
+                            //Initialize address list
+                            val addresses = geocoder.getFromLocation(
+                                location.latitude, location.longitude, 1
+                            )
+                            //Set latitude on TextView
+                            binding.latitude.setText("Latitude: " + addresses[0].latitude)
+                            //Set longitude on TextView
+                            binding.longitude.setText("Longitude: " + addresses[0].longitude)
+                            //Set country on TextView
+                            binding.country.setText("Country: " + addresses[0].countryName)
+                            //Set city on TextView
+                            binding.city.setText("City: " + addresses[0].locality)
+                            //Set address on TextView
+                            binding.address.setText("Address: " + addresses[0].getAddressLine(0))
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            })
     }
 }
--- app/src/main/java/com/example/projecta/MainActivity.kt
+++ app/src/main/java/com/example/projecta/MainActivity.kt
@@ -1,27 +1,69 @@
 package com.example.projecta
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.SENSOR_SERVICE
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.Gravity
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.motion.widget.Debug.getLocation
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.projecta.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.navigation.NavigationView
+import java.util.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var sensorManager: SensorManager
+    private lateinit var square: TextView
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private val permissionId = 2
+    private lateinit var tts: TextToSpeech
+    private lateinit var latitude: TextView
+    private lateinit var longitude: TextView
+    private lateinit var country: TextView
+    private lateinit var city: TextView
+    private lateinit var address: TextView
+    private lateinit var btnSpeak: Button
+    private lateinit var button: Button
+    private lateinit var btnLocation: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
 
         toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
@@ -29,7 +71,7 @@ class MainActivity : AppCompatActivity(){
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+        replaceFragment(HomeFragment(), "Home")
         binding.navView.setNavigationItemSelectedListener {
             it.isChecked = true
             replaceFragment(HomeFragment(), it.title.toString())
@@ -44,22 +86,25 @@ class MainActivity : AppCompatActivity(){
             }
             true
         }
+
     }
 
-    private fun replaceFragment(fragment: Fragment, title: String){
+    private fun replaceFragment(fragment: Fragment, title: String) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
         binding.drawerLayout.closeDrawers()
         setTitle(title)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)){
+        if (toggle.onOptionsItemSelected(item)) {
             return true
-        } else{
+        } else {
             return super.onOptionsItemSelected(item)
         }
     }
+
+
 }
